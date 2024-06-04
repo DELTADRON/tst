85775de6641c8c51ecca95046aa7ae3883ece4c2
DELTADRON <79601050+DELTADRON@users.noreply.github.com>
2024-06-04 18:01:32 +0200

    Add files via upload

---
+++ cmda/install.cmd
@@ -0,0 +1,27 @@
+@set app.id=cmda_installer
+@set app.name=CMDA installer
+@set app.start=%cd%
+@set app.temp=%temp%\cmda\apps\%app.id%
+
+@title %name%
+@chcp 65001 >nul
+@mode 91,25
+@color 07
+@echo off
+
+if not exist "%app.temp%" mkdir "%app.temp%"
+if not exist "%temp%\cmda\ext" mkdir "%temp%\cmda\ext"
+(echo %cd%)>"%app.temp%\locate"
+
+call :get_file "https://deltadron.github.io/tst/cmda/cmda.zip" "%app.temp%\install.zip"
+call :get_file "https://deltadron.github.io/tst/cmda/installer.cmd" "%app.temp%\install.cmd"
+timeout /t 1>nul
+call explorer "%app.temp%\install.cmd"
+
+goto 0
+
+:get_file
+bitsadmin /Transfer DownFile "%1" "%2"
+exit /b
+
+:0
---
+++ cmda/installer.cmd
@@ -0,0 +1,30 @@
+@set cmda.temp=%temp%\cmda
+@set cmda.home=%programfiles%\cmda
+@echo off
+
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+
+
+(echo %cmda.home%)>"%cmda.temp%\locate"
+
+if exist "%cmda.home%" (
+    del /q destination\*
+    del /q destination
+)
+
+cd "%cmda.temp%"
+
+call :unzip "%app.temp%\install.zip" "%programfiles%"
+setx /M path "%path%;%cmda.home%"
+msg 0 "Installed"
+
+goto 0
+
+:unzip
+powershell -nologo -noprofile -command "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('%2'); $zip = $shell.NameSpace('%1'); $target.CopyHere($zip.Items(), 16); }"
+exit /b
+
+:0
