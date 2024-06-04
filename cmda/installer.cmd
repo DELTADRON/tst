62ce76b2dd3d3aa89cdfae9f353e629e206d4bde
DELTADRON <79601050+DELTADRON@users.noreply.github.com>
2024-06-04 18:34:20 +0200

    Add files via upload

--- cmda/installer.cmd
+++ cmda/installer.cmd
@@ -1,4 +1,3 @@
-@set cmda.temp=%temp%\cmda
 @set cmda.home=%programfiles%\cmda
 @echo off
 
@@ -6,33 +5,22 @@ if not "%1"=="am_admin" (
     powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
     exit /b
 )
-
-
-(echo %cmda.home%)>"%cmda.temp%\locate"
+(echo %cmda.home%)>"%temp%\cmda\locate"
 
 if exist "%cmda.home%" (
     del /q "%cmda.home%\*"
     del /q "%cmda.home%"
 )
 
-cd "%cmda.temp%"
-
-::call :unzip "%app.temp%\install.zip" "%cmda.temp%"
-call :move "%cmda.temp%\cmda" "%programfiles%"
-setx /M path "%path%;%cmda.home%"
+call :move "%temp%\cmda\cmda" "%programfiles%"
+setx /M path "%path:C:\Program Files\cmda=%"
+setx /M path "%path%;%programfiles%\cmda"
 timeout /t 1>nul
 msg 0 "Installed"
-del /q "%cmda.home%\*" & exit
+del /q "%temp%\cmda\apps\cmda_installer\*"
+del /q "%temp%\cmda\apps\cmda_installer"
 
 goto 0
-
-:unzip
-setlocal
-set file=%1
-set dr=%2
-powershell -nologo -noprofile -command "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('%dr%'); $zip = $shell.NameSpace('%file%'); $target.CopyHere($zip.Items(), 16); }"
-endlocal
-exit /b
 
 :move
 for %%a in ("%1\*") do move /y "%%~fa" %2
