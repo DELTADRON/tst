22ada85f78aae22ee1e31fc66cbfa537b0f52fe1
DELTADRON <79601050+DELTADRON@users.noreply.github.com>
2024-06-04 18:56:11 +0200

    Add files via upload

--- cmda/install2.cmd
+++ cmda/install2.cmd
@@ -1,16 +1,18 @@
-@set cmda.home=%programfiles%\cmda
 if not "%1"=="am_admin" (
     powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
     exit /b
 )
-(echo %cmda.home%)>"%temp%\cmda\locate"
-if exist "%cmda.home%" erase /s /q "%cmda.home%"
-for %%a in ("%temp%\cmda\cmda\*") do move /y "%%~fa" "%programfiles%"
-for /d %%a in ("%temp%\cmda\cmda\*") do move /y "%%~fa" "%programfiles%"
+(echo %programfiles%\cmda)>"%temp%\cmda\locate"
+if exist "%programfiles%\cmda" erase /s /q "%programfiles%\cmda"
+mkdir "%programfiles%\cmda"
+for %%a in ("%temp%\cmda\cmda\*") do move /y "%%~fa" "%programfiles%\cmda"
+for /d %%a in ("%temp%\cmda\cmda\*") do move /y "%%~fa" "%programfiles%\cmda"
 setx /M path "%path:;C:\Program Files\cmda=%"
 setx /M path "%path%;%programfiles%\cmda"
 setx /M path "%path%;%programfiles%\cmda\lib"
 timeout /t 1>nul
 msg 0 "Installed"
 erase /s /q "%temp%\cmda\apps\cmda_installer"
-if exist "%temp%\cmda\cmda" erase /s /q "%temp%\cmda\cmda"
+erase /s /q "%temp%\cmda\cmda"
+del /Q "%temp%\cmda\apps\cmda_installer"
+del /Q "%temp%\cmda\cmda"
