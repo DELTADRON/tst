38b89c88842f24275e2c62c9e8359c1d7ede27c9
DELTADRON <79601050+DELTADRON@users.noreply.github.com>
2025-05-30 06:25:28 +0200

    Update i.js

--- i.js
+++ i.js
@@ -1,1 +1,14 @@
+//window.document.body.style.background = "#000";
 
+let link = document.createElement('link');
+// set the attributes for link element
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = 'https://raw.githubusercontent.com/DELTADRON/tst/refs/heads/main/i.css';
+// Get HTML head element to append
+// link element to it
+document.getElementsByTagName('HEAD')[0].appendChild(link);
+
+//window.document.querySelectorAll('div').forEach(el => {
+//  if (el.style.background==="#ffffff") el.style.background="#000";
+//})
